80993a4a4d8dca5ecd05abde1fbb807b0263b703
Kevin <kevindemaria95@gmail.com>
2023-12-07 14:41:42 +0530

    some code fix

--- controller/AWS-File-Upload/file-upload/file-upload.controller.js
+++ controller/AWS-File-Upload/file-upload/file-upload.controller.js
@@ -2,6 +2,20 @@ const uploadService = require('../../../services/AWS-Services/file-upload/file-u
 
 const upload = async (req, res) => {
   try {
+    if (req.file == undefined) {
+      return res.status(400).json({ messge: 'File is missing' });
+    }
+
+    const { id, title, description } = req.body;
+
+    if (id == undefined) {
+      return res.status(400).json({ message: 'id is required' });
+    } else if (title == undefined) {
+      return res.status(400).json({ message: 'title is required' });
+    } else if (description == undefined) {
+      return res.status(400).json({ message: 'description is required' });
+    }
+
     const uploadReponse = await uploadService(req);
     if (uploadReponse && uploadReponse?.code !== 400) {
       res.status(200).json(uploadReponse);
--- todo.md
+++ todo.md
@@ -1,1 +1,7 @@
-# Fix the issue in file-upload service api and host
+### File Upload
+
+# 1. Handle all user input
+
+# 2. If user input is not the return the respective field missing
+
+# 3.
