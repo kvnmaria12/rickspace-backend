355fb804ae4acb131daa8403c5fd6cb3a0db7de0
Kevin <kevindemaria95@gmail.com>
2023-12-07 17:11:35 +0530

    add prisma info

--- repository/AWS-Repository/get-posts/get-posts.repo.js
+++ repository/AWS-Repository/get-posts/get-posts.repo.js
@@ -1,11 +1,18 @@
 const { PrismaClient } = require('@prisma/client');
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  log: ['query', 'info', 'warn', 'error'],
+});
 
 const getAllPostsRepo = async (req) => {
   try {
     const { authorId } = req.body;
-    const posts = await prisma.post.findRaw();
+
+    const posts = await prisma.post.findMany({
+      where: {
+        authorId: authorId,
+      },
+    });
     console.log('posts', posts);
     return posts;
   } catch (error) {
