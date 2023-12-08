eca3e001d3e4a39d44fb6e5df57c2b3bd66b700c
Kevin <kevindemaria95@gmail.com>
2023-12-08 11:18:16 +0530

    change the prisma schema

--- repository/AWS-Repository/get-posts/get-posts.repo.js
+++ repository/AWS-Repository/get-posts/get-posts.repo.js
@@ -5,9 +5,11 @@ const prisma = new PrismaClient();
 const getAllPostsRepo = async (req) => {
   try {
     const { authorId } = req.body;
-
-    const posts = await prisma.menu.findMany();
-    console.log('posts', posts);
+    const posts = await prisma.post.findMany({
+      where: {
+        authorId: authorId,
+      },
+    });
     return posts;
   } catch (error) {
     console.log('error --->', error);
