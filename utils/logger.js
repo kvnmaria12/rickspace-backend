90d40415a474032fc8f1f3ae42189e7278ff273e
Kevin <kevindemaria95@gmail.com>
2024-01-18 12:51:54 +0530

    remove unwanted code

--- utils/logger.js
+++ utils/logger.js
@@ -1,15 +1,10 @@
 const winston = require('winston');
 
-const { combine, timestamp, label, prettyPrint, colorize } = winston.format;
+const { combine, timestamp, prettyPrint, colorize } = winston.format;
 
 const logger = winston.createLogger({
   level: 'info',
-  format: combine(
-    // label({ label: 'right meow!' }),
-    timestamp(),
-    prettyPrint(),
-    colorize({ all: true })
-  ),
+  format: combine(timestamp(), prettyPrint(), colorize({ all: true })),
   transports: [new winston.transports.Console()],
 });
 
