480fec58dd45ffeed2f28e31ccd4a26db7eba42f
Kevin <kevindemaria95@gmail.com>
2024-01-24 17:29:27 +0530

    change api limit to 30

--- utils/limiter.js
+++ utils/limiter.js
@@ -2,7 +2,7 @@ const { rateLimit } = require('express-rate-limit');
 
 const limiter = rateLimit({
   windowMs: 180000, // 1minute
-  max: 10, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  max: 30, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
   message: (req, res) => {
