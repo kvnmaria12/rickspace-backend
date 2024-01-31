e6ecbb467e3a9eeccbc3e9e9002a932e68b2be9f
Kevin <kevindemaria95@gmail.com>
2024-01-31 14:17:07 +0530

    add validation error

--- routes/comments/comments.route.js
+++ routes/comments/comments.route.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 const addCommentController = require('../../controller/comments/add-comment/add-comment.controller');
 const updateCommentController = require('../../controller/comments/update-comment/update-comment.controller');
-const commentValidator = require('../../validators/Comment/comment');
+const commentValidator = require('../../validators/Comment/add-comment');
 const deleteCommentController = require('../../controller/comments/delete-comment/delete-comment.controller');
+const updateCommentValidator = require('../../validators/Comment/update-comment');
 
 router.post('/add-comment', commentValidator, addCommentController);
-router.post('/update-comment', commentValidator, updateCommentController);
+router.post('/update-comment', updateCommentValidator, updateCommentController);
 router.delete('/delete-comment', deleteCommentController);
 module.exports = router;
